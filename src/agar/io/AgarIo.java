54532694cf1e700f605cac444cf4739c3a7e4792
Ivan <vandamkakdam1@gmail.com>
2016-10-30 18:54:21 +0300

    change gitignore, added the file with gtge library

--- src/agar/io/AgarIo.java
+++ src/agar/io/AgarIo.java
@@ -3,25 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package agario;
+package agar.io;
 
 import com.golden.gamedev.GameLoader;
-import com.golden.gamedev.funbox.GameSettings;
 import java.awt.Dimension;
 import views.GameView;
-import views.SettingsView;
 
 /**
  *
  * @author 999
  */
-public class AgarIO {
+public class AgarIo {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //new SettingsView();
         GameLoader game = new GameLoader();
         game.setup(new GameView(), new Dimension(800,600), false);//Инициализация графического движка
         game.start();
