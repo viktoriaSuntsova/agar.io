5224d2fdd3c43a97d1eaf8b59010970c6820b426
homepc <vandamkakdam1@gmail.com>
2017-03-09 17:11:54 +0300

    вынесены разные проекты под разные библиотеки

---
+++ engines/gtge/src/engines/views/GameView.java
@@ -0,0 +1,34 @@
+package engines.views;
+
+import com.golden.gamedev.Game;
+import java.awt.Graphics2D;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author homepc
+ */
+public class GameView extends Game {
+
+    @Override
+    public void initResources() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(long l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void render(Graphics2D gd) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
